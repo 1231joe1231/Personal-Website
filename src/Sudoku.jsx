42a7cc4d4226941e0d0eae02314721cb0de453e7
Joe <zhuangruixin1231@gmail.com>
2021-06-30 01:47:31 +0800

    Lint files and add sudoku library

--- src/Sudoku.jsx
+++ src/Sudoku.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 
 class Sudoku extends React.Component {
   render() {
     return (
       <div>
         <AppBar position="static">
-          <Toolbar  style={{
+          <Toolbar
+            style={{
               display: "flex",
-              justifyContent: "space-between"
-            }}>
-            <Typography variant="h6">
-              News
-            </Typography>
-            <Button color="inherit" style={{marginRight: "10px"}}>Login</Button>
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="h6">News</Typography>
+            <Button color="inherit" style={{ marginRight: "10px" }}>
+              Login
+            </Button>
           </Toolbar>
         </AppBar>
-        <Container maxWidth="lg">
-          <Box style={{
-            display: "flex",
-            justifyContent: "space-between",
-            padding: "20px"
-          }}>
+        <Container maxWidth="md">
+          <Box
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              padding: "20px",
+            }}
+          >
             <Button variant="contained" color="primary">
               解答
             </Button>
