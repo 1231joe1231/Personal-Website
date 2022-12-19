680ad437e43276902fd74a990e13f2e372e167a3
1231joe1231 <zhuangruixin1231@gmail.com>
2022-12-19 04:46:06 -0500

    Huge progress, website almost roughly done

--- frontend/src/Component/ImageCarousel.jsx
+++ frontend/src/Component/ImageCarousel.jsx
@@ -1,76 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import LinearProgress from "@mui/material/LinearProgress";
+// import LinearProgress from "@mui/material/LinearProgress";
+import axios from "axios";
 
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-    author: "@bkristastucchio",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-    author: "@rollelflex_graphy726",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-    author: "@helloimnik",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-    author: "@nolanissac",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    author: "@hjrc33",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-    author: "@tjdragotta",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-    author: "@katie_wasserman",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    author: "@silverdalex",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-    author: "@shelleypauls",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-    author: "@peterlaster",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    author: "@southside_customs",
-  },
-];
+const backend = axios.create({
+  // always get image from server
+  baseURL: "https://joe-zhuang.com/api",
+});
 
 export default function ImageCarousel() {
   // const [progress, setProgress] = React.useState(0);
+  const [imageArr, setImageArr] = useState([]);
+
+  const fetchImages = () => {
+    backend
+      .get("/images/recent", { params: { count: 5 } })
+      .then((response) => setImageArr(response.data));
+  };
+
+  useEffect(() => {
+    // fetch notes from backend
+    fetchImages();
+  }, []);
 
   return (
     <Carousel
@@ -84,15 +39,15 @@ export default function ImageCarousel() {
       NextIcon={<ChevronRightIcon fontSize="large" />}
       PrevIcon={<ChevronLeftIcon fontSize="large" />}
     >
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
+      {imageArr.map((item) => (
+        <ImageListItem key={item.id}>
           <img
-            src={item.img}
+            src={item.path}
             alt={item.title}
             loading="lazy"
             style={{ height: "40vh", width: "100%", objectFit: "cover" }}
           />
-          <LinearProgress
+          {/* <LinearProgress
             variant="determinate"
             style={{
               position: "absolute",
@@ -101,7 +56,7 @@ export default function ImageCarousel() {
               right: 0,
               height: "4px",
             }}
-          />
+          /> */}
           <ImageListItemBar
             sx={{
               background:
--- frontend/src/Home/index.jsx
+++ frontend/src/Home/index.jsx
@@ -2,13 +2,14 @@
 import React, { useState, useEffect } from "react";
 import AppBar from "../Component/AppBar";
 import ImageCarousel from "../Component/ImageCarousel";
-// import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import QuicknoteCard from "../Component/QuicknoteCard";
 import axios from "axios";
 import { BACKEND_URL } from "../env";
@@ -30,10 +31,16 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <div style={{ height: "100vh", backgroundColor: "#F5F5F5" }}>
       <AppBar title="Home" />
-      <Container maxWidth="90vw">
-        <Grid container spacing={5} sx={{ padding: "20px" }}>
+      <Container
+        maxWidth="90vw"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Grid container spacing={5} sx={{ margin: "0px" }}>
           <Grid
             container
             xl={4}
@@ -42,12 +49,110 @@ export default function Home() {
             sx={{ flexDirection: "column" }}
           >
             <Grid xs={12}>
-              <Typography variant="h5" sx={{ margin: "20px" }}>
+              <Typography variant="h5" sx={{ marginBottom: "20px" }}>
                 About me
               </Typography>
+              <Paper elevation={3}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    padding: "20px",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Avatar
+                    sx={{ width: "15vh", height: "15vh" }}
+                    src="/path/to/avatar.jpg"
+                  />
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      marginLeft: "20px",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: 30 }}>
+                      Hello, I'm Ruixin(Joe) Zhuang
+                    </Typography>
+                    <List
+                      dense
+                      sx={{
+                        listStyleType: "disc",
+                        pl: 2,
+                        "& .MuiListItem-root": {
+                          display: "list-item",
+                        },
+                      }}
+                    >
+                      <ListItem dense alignItems="flex-start">
+                        <Typography>
+                          👋 Hi, I'm Ruixin(Joe) Zhuang, a recent graduate from
+                          Computer Science program at University of Toronto
+                          Scarborough
+                        </Typography>
+                      </ListItem>
+                      <ListItem dense alignItems="flex-start">
+                        <Typography>
+                          👀 I’m interested in coding, photography, films,
+                          camping, guitar
+                        </Typography>
+                      </ListItem>
+                      <ListItem dense alignItems="flex-start">
+                        <Typography>
+                          🌱 I’m currently learning Node.js, DevOps, React
+                        </Typography>
+                      </ListItem>
+                      <ListItem dense alignItems="flex-start">
+                        <Typography>
+                          📬 How to reach me: Click the link below!
+                        </Typography>
+                      </ListItem>
+                    </List>
+                    <div
+                      style={{
+                        display: "flex",
+                        marginTop: "10px",
+                        justifyContent: "space-around",
+                      }}
+                    >
+                      <Button
+                        variant="contained"
+                        href="https://www.linkedin.com/in/ruixin-zhuang/"
+                        sx={{ textTransform: "none" }}
+                      >
+                        LinkedIn
+                      </Button>
+                      <Button
+                        variant="contained"
+                        href="https://github.com/1231joe1231"
+                        sx={{ textTransform: "none" }}
+                      >
+                        Github
+                      </Button>
+                      <Button
+                        variant="contained"
+                        href="https://space.bilibili.com/8190716"
+                        sx={{ textTransform: "none" }}
+                      >
+                        Bilibili
+                      </Button>
+                      <Button
+                        variant="contained"
+                        href="/projects"
+                        sx={{ textTransform: "none" }}
+                      >
+                        Resume
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </Paper>
             </Grid>
             <Grid xs={12}>
-              <Typography variant="h5" sx={{ margin: "20px" }}>
+              <Typography variant="h5" sx={{ marginBottom: "20px" }}>
                 Recent photography works
               </Typography>
               <ImageCarousel />
@@ -55,7 +160,7 @@ export default function Home() {
           </Grid>
           <Grid container xl={8} md={6} xs={12}>
             <Grid xl={6} md={12} xs={12}>
-              <Typography variant="h5" sx={{ margin: "20px" }}>
+              <Typography variant="h5" sx={{ marginBottom: "20px" }}>
                 Recent quick notes
               </Typography>
               <Paper elevation={3}>
@@ -64,7 +169,22 @@ export default function Home() {
                     padding: "20px",
                   }}
                 >
-                  <List sx={{ height: "75vh", overflow: "auto" }}>
+                  <List
+                    sx={{
+                      height: "75vh",
+                      overflow: "auto",
+                      scrollbarWidth: "thin",
+                      "&::-webkit-scrollbar": {
+                        width: "0.4em",
+                      },
+                      "&::-webkit-scrollbar-thumb": {
+                        backgroundColor: "#888",
+                      },
+                      "&::-webkit-scrollbar-thumb:hover": {
+                        background: "#555",
+                      },
+                    }}
+                  >
                     {notesArr.map((note) => (
                       <ListItem key={note.id}>
                         <QuicknoteCard
@@ -81,7 +201,7 @@ export default function Home() {
               </Paper>
             </Grid>
             <Grid xl={6} md={12} xs={12}>
-              <Typography variant="h5" sx={{ margin: "20px" }}>
+              <Typography variant="h5" sx={{ marginBottom: "20px" }}>
                 Recent articles
               </Typography>
               <Paper elevation={3}>
@@ -90,7 +210,22 @@ export default function Home() {
                     padding: "20px",
                   }}
                 >
-                  <List sx={{ height: "75vh", overflow: "auto" }}>
+                  <List
+                    sx={{
+                      height: "75vh",
+                      overflow: "auto",
+                      scrollbarWidth: "thin",
+                      "&::-webkit-scrollbar": {
+                        width: "0.4em",
+                      },
+                      "&::-webkit-scrollbar-thumb": {
+                        backgroundColor: "#888",
+                      },
+                      "&::-webkit-scrollbar-thumb:hover": {
+                        background: "#555",
+                      },
+                    }}
+                  >
                     {notesArr.map((note) => (
                       <ListItem key={note.id}>
                         <QuicknoteCard
