e5d2d0f9c0553a63056e135e0dbb50d64e517872
1231joe1231 <zhuangruixin1231@gmail.com>
2023-02-12 04:26:02 -0500

    Display article page

--- frontend/src/TechBlog/article.jsx
+++ frontend/src/TechBlog/article.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "../Component/AppBar";
 import { useParams } from "react-router-dom";
+import { BACKEND_URL } from "../env";
+import axios from "axios";
+
+const backend = axios.create({
+  baseURL: BACKEND_URL,
+});
 
 export default function Article() {
   let { id } = useParams();
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteContent, setNoteContent] = useState("");
+
+  const fetchNote = () => {
+    backend.get("/notes", { params: { id } }).then((response) => {
+      setNoteTitle(response.data["title"]);
+      setNoteContent(response.data["content"]);
+    });
+  };
+
+  useEffect(() => {
+    // fetch note from backend
+    fetchNote();
+  }, []);
   return (
     <div>
       <AppBar title="Article" />
       <div>
-        <h3>ID: {id}</h3>
+        <h3>{noteTitle}</h3>
+        <h5>{noteContent}</h5>
       </div>
     </div>
   );
